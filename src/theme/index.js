50e8b8ddf3caeea76089af4e7902819990548d9a
Akdiel <Akdiel.callejas@gmail.com>
2021-03-01 23:08:10 -0600

    Creando la base del archivo para la selección de los temas

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+      primary: "#607D8B",
+      secondary: "#4CAF50",
+      backgroundDark: "#171c26",
+      purple: "#ae3b76",
+      green: "#0aebaf",
+      orange: "#ff7746",
+    },
+  };
+  
+  export default theme;
