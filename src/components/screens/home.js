b1d268425ee6e44fdf98575bd564ee40bd7d27cd
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-03-02 23:23:06 -0600

    Inicio de la Pantalla Home

---
+++ src/components/screens/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text } from "react-native";
+
+
+const { width, height } = Dimensions.get("screen");
+
+const Home = () => {
+  return (
+    <Text>Hola</Text>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
