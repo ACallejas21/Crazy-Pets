374fcf048cb28ed03646808ceeba056a3f4b3813
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-03-02 23:26:15 -0600

    Inicio de la pantalla agregar mascotas (addPets)

---
+++ src/components/screens/addPets.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text } from "react-native";
+
+
+const { width, height } = Dimensions.get("screen");
+
+const AddPets = () => {
+  return (
+    <Text>Hola</Text>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AddPets;
