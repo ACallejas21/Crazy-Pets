660bee28a6cc0f7e2c44b8f48e5c34d8080e5742
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-03-05 17:14:18 -0600

    Creacion de función para recuperar contraseña

--- src/components/screens/passwordRecovery.js
+++ src/components/screens/passwordRecovery.js
@@ -1,15 +1,36 @@
-import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import { Input, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { validate } from "email-validator";
+import {firebase} from "../../firebase";
+import Alert from "../shared/alert";
+import "@firebase/auth";
 
 
 const { width, height } = Dimensions.get("screen");
 
 const PasswordRecovery = () => {
+  const [password, setPassword] = useState("");
+
+  const handlePasswordRecovery = () => {
+   firebase.auth().sendPasswordResetEmail(password).then(function() {
+    console.log("ENviado")
+  }).catch(function(error) {
+    console.log("error")
+  });
+  }
+
   return (
-    <Text>Hola</Text>
+    <View>
+    <Input
+    placeholder="Contraseña"
+    leftIcon={<Icon name="lock" />}
+    value={password}
+    onChangeText={setPassword}
+    />
+  <Button title="Iniciar Sesión" onPress={handlePasswordRecovery} />
+  </View>
   );
 };
 
