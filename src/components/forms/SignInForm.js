c194acbbea932ea022b567dcac2d0c924d06ad43
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-03-05 17:16:11 -0600

    Modificaciones en la pantalla y componente de inicio de sesion

--- src/components/forms/SignInForm.js
+++ src/components/forms/SignInForm.js
@@ -1,34 +1,80 @@
 import React, { useState } from "react";
-import { StyleSheet, 
-        View } from "react-native";
-import { Input, 
-        Button } from "react-native-elements";
+import { StyleSheet, View } from "react-native";
+import { Input, Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { validate } from "email-validator";
+import {firebase} from "../../firebase";
+import Alert from "../shared/alert";
 
-const SigninForm = () => {
+const SigninForm = ({navigation}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  // Verifica que se ingresan los datos del email y el password
+  const handleVerify = (input) => {
+    if (input === "email") {
+      if (!email) setEmailError(true);
+      else if (!validate(email)) setEmailError(true);
+      else setEmailError(false);
+    } else if (input === "password") {
+      if (!password) setPasswordError(true);
+      else setPasswordError(false);
+    }
+  };
+
+  const handleSignin = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => { 
+      
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      }); 
+          
+  };
 
   return (
     <View>
+      {error ? <Alert title={error} type="error" /> : null}
       <Input
-        placeholder="Usuario"
+        placeholder="Correo"
         leftIcon={<Icon name="envelope" />}
         value={email}
-        onChange={setEmail}
+        onChangeText={setEmail}
+        onBlur={() => {
+          handleVerify("email");
+        }}
+        errorMessage={
+          emailError
+            ? "Por favor ingresa tu cuenta de correo electrónico"
+            : null
+        }
       />
       <Input
         placeholder="Contraseña"
         leftIcon={<Icon name="lock" />}
         value={password}
-        onChange={setPassword}
+        onChangeText={setPassword}
+        onBlur={() => {
+          handleVerify("password");
+        }}
+        errorMessage={passwordError ? "Por favor ingresa tu contraseña" : null}
       />
-      <Button title="Inicio Sesión" />
+      <Button title="Iniciar Sesión" onPress={handleSignin} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttom:{
+    borderRadius: 45,
+  },
+});
 
 export default SigninForm;
--- src/components/screens/SignIn.js
+++ src/components/screens/SignIn.js
@@ -1,22 +1,24 @@
 import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  Text } from "react-native";
-import { Input, Button } from "react-native-elements";
+import { Dimensions,  StyleSheet, TouchableOpacity, View, Text } from "react-native";
 import Logo from "../shared/logo";
 import SigninForm from "../forms/SignInForm";
+import theme from "../../theme/index";
+import Alert from "../shared/alert";
 
 const { width, height } = Dimensions.get("screen");
 
-const Login = ({ navigation }) => {
+const Login = ({ navigation, route }) => {
+  const { userCreated } = route.params;
   return (
     <View style={styles.container}>
       <Logo />
+      {userCreated ? (
+        <Alert type="success" title="User created! You can now sign in!" />
+      ) : null}
       <SigninForm />
+      <TouchableOpacity onPress={() => navigation.navigate("PasswordRecovery")}>
         <Text style={styles.forgotPassword}>Recuperación de contraseña</Text>
+        </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
             <Text style={styles.signUp}>Registrate</Text>
         </TouchableOpacity>
@@ -28,16 +30,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    padding: 10,
+    padding: 15,
+    backgroundColor: theme.colors.backgroundWhite,
   },
   forgotPassword: {
     textAlign: "center",
-    padding: 5,
+   
   },
   signUp: {
     textAlign: "center",
-    padding: 50,
+  
   },
+
 });
 
 export default Login;
