de6b32f204c3065daf53fab215d40724c748511f
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-03-07 20:45:35 -0600

    Se manda a llamar el formulario de recuperacion de contraseña

--- src/components/screens/passwordRecovery.js
+++ src/components/screens/passwordRecovery.js
@@ -1,39 +1,35 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
-import { Input, Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { validate } from "email-validator";
-import {firebase} from "../../firebase";
-import Alert from "../shared/alert";
-import "@firebase/auth";
-
+import { StyleSheet, View, Dimensions, TouchableOpacity, Text } from "react-native";
+import theme from "../../theme/index";
+import Logo from "../shared/logo";
+import PasswordRecoveryForm from "../forms/passwordRecoveryForm";
 
 const { width, height } = Dimensions.get("screen");
 
-const PasswordRecovery = () => {
-  const [password, setPassword] = useState("");
-
-  const handlePasswordRecovery = () => {
-   firebase.auth().sendPasswordResetEmail(password).then(function() {
-    console.log("ENviado")
-  }).catch(function(error) {
-    console.log("error")
-  });
-  }
-
-  return (
-    <View>
-    <Input
-    placeholder="Contraseña"
-    leftIcon={<Icon name="lock" />}
-    value={password}
-    onChangeText={setPassword}
-    />
-  <Button title="Iniciar Sesión" onPress={handlePasswordRecovery} />
-  </View>
+const PasswordRecovery = ({navigation}) => {
+   return (
+    <View style={styles.container}>
+      <Logo/>
+      <PasswordRecoveryForm />
+      <TouchableOpacity onPress={() => {navigation.goBack()}}>
+      <Text style={styles.Text}>Inicio Sesión</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 15,
+    backgroundColor: theme.colors.backgroundWhite,
+  },
+  Text:{
+    textAlign: "center",
+    marginTop: width*0.03,
+    fontSize: 15,
+  }
+});
 
 export default PasswordRecovery;
