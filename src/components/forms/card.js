c78f92ad08435a47dc651aee6880dca5db83de30
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-03-10 00:56:05 -0600

    Card para la pantalla de home

---
+++ src/components/forms/card.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { StyleSheet, View, Text, Dimensions, Button } from "react-native";
+import {Card, Image} from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const { width, height } = Dimensions.get("screen");
+
+const PetsCard = ({}) => {
+  return (
+    <View style={styles.container}>
+        <View style={styles.card}>
+            <View style={{flex: 0.4 }}>
+                <Image style={styles.logo}
+                source={ require("../../../assets/logo.png")}/>
+            </View>
+            <View style={{ flex: 0.6 }}>
+                <Text style={styles.nombre}>Sazu</Text>
+                <Text>Hay determinados tipos de loros que hablan. 
+                    Estas aves son animales muy inteligentes y sociables, con la repetición de sonidos 
+                    o expresiones
+                </Text>
+                <Icon style={styles.icon} name="arrow-right"></Icon>
+            </View>
+        </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+
+  logo: {
+    width: width * 0.27,
+    height: height * 0.155,
+    resizeMode: "contain",
+    marginLeft:3,
+  },
+  titulo: {
+      textAlign: "center",
+      fontSize: 30,
+
+  },
+  card:{
+        flexDirection: "row",
+        height: height*0.19,
+        width: width*0.91,
+        marginRight: width * 0.02,
+        backgroundColor: "#fff",
+        borderColor: "#000",
+        borderWidth: 2,
+        borderRadius: 15,
+  },
+  nombre:{
+      fontSize: 18,
+      textAlign: "center",
+  },
+  icon:{
+    fontSize: 24,
+    color:"black",
+    textAlign: "right",
+    marginRight: 5,
+  }
+});
+
+export default PetsCard;
