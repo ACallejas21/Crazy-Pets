67a4ac2ebd47efa67eddf4896582a8af1b3f4364
Akdiel <Akdiel.callejas@gmail.com>
2021-04-08 00:29:40 -0600

    mantalla para agregar la mascota

--- src/components/screens/addPets.js
+++ src/components/screens/addPets.js
@@ -1,18 +1,44 @@
-import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  Text } from "react-native";
+import React, { useContext, useState } from "react";
+import { StyleSheet, View ,Button} from "react-native";
+import theme from "../../theme";
+import { Context as AuthContext } from "../../providers/AuthContext";
+import { Context as PetsContext } from "../../providers/PetsContext";
+import PetForm from "../forms/addPetForm.js"
 
+const AddPet = ({ navigation }) => {
+  const { createPet } = useContext(PetsContext);
+  const { state } = useContext(AuthContext);
 
-const { width, height } = Dimensions.get("screen");
+ 
 
-const AddPets = () => {
   return (
-    <Text>Hola</Text>
+    <View style={styles.container}>
+      <PetForm navigation={navigation}/>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: theme.colors.backgroundWhite,
+  },
+  titleInput: {
+    fontSize: 22,
+    fontWeight: "bold",
+    backgroundColor: theme.colors.backgroundWhite,
+  },
+  contentInput: {
+    flex: 1,
+    backgroundColor: theme.colors.backgroundWhite,
+    borderBottomWidth: 0,
+  },
+  iconBar: {
+    paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+});
 
-export default AddPets;
+export default AddPet;
