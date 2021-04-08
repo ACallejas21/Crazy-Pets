9e6cf1911706fd510ef524d50bfaa31554262a3d
Jcastellanos99 <karolcastellanos99@gmail.com>
2021-04-08 00:34:52 -0600

    Se mandan a traer los datos del usuario

--- src/components/screens/profile.js
+++ src/components/screens/profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext} from "react";
 import {
   Dimensions,
   StyleSheet,
@@ -7,6 +7,7 @@ import {
 import Logo from "../shared/logo"
 import {Text, Button} from "react-native-elements"
 import {firebase} from "../../firebase"
+import {Context as AuthContext} from "../../providers/AuthContext";
 
 
 
@@ -14,25 +15,22 @@ const { width, height } = Dimensions.get("screen");
 
 const Profile = ({navigation}) => {
 
-    const handleSignOut = () => {
-        firebase.auth().signOut().then(() => {
-          navigation.navigate("Signin");
-          console.log("Cerrar Sesion");
-        }).catch((error) => {
-          console.log("Error");
-        });
-              
-      };
+  const { state, signout } = useContext(AuthContext);
+
+
+
+
+
 
   return (
     <View>
         <Logo/>
         <Text h4 style={styles.text}>Usuario</Text>
-        <Text h5 style={styles.text}>Nombre del usuario</Text>
+        <Text h5 style={styles.text}>{state.user.user}</Text>
         <Text h4 style={styles.text}>Correo</Text>
-        <Text h5 style={styles.text}>Correo del usuario</Text>
+        <Text h5 style={styles.text}>{state.user.email}</Text>
 
-        <Button style={styles.button} title="Cerrar Sesión" onPress={handleSignOut} />
+        <Button style={styles.button} title="Cerrar Sesión" onPress={() => { signout();  }} />
     </View>
 
   );
