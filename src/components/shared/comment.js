1dff07c06932d9f7cb0129e53fdba5bb46a67d07
Akdiel <Akdiel.callejas@gmail.com>
2021-04-11 13:48:27 -0600

    compente del comentario

---
+++ src/components/shared/comment.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { StyleSheet, View, Text, Dimensions, Button } from "react-native";
+import {Card, Image} from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const { width, height } = Dimensions.get("screen");
+
+const comment = ({usuario, comentario}) => {
+  return (
+    <View style={styles.container}>
+        <View style={styles.card}>
+            <Text style={styles.usuario} >{usuario}</Text>
+            <Text style={styles.comentario} >{comentario}</Text>
+        </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  usuario: {
+      textAlign: "left",
+      fontSize: 15,
+      marginTop:5,
+  },
+  comentario: {
+    fontSize: 20,
+    marginTop:30,
+},
+card:{
+    flexDirection: "row",
+    height: height*0.19,
+    width: width*0.91,
+    marginRight: width * 0.02,
+    backgroundColor: "#fff",
+    borderColor: "#000",
+    borderWidth: 2,
+    borderRadius: 15,
+},
+
+
+
+});
+
+export default comment;
