6b8efdd5d432eef214fe0e73f6bd58c53fa2c54c
Akdiel <Akdiel.callejas@gmail.com>
2021-04-11 13:48:14 -0600

    implementacion de contexto de tips

--- src/components/screens/tips.js
+++ src/components/screens/tips.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useContext, useEffect } from "react";
 import { StyleSheet, View, Dimensions} from "react-native";
 import { Input, Button, Text,SocialIcon } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -7,57 +7,42 @@ import { Fontisto } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import Logo from "../shared/logo"
+import CommentList from "../shared/commentList";
+import { Context as AuthContext } from "../../providers/AuthContext";
+import { Context as TipsContext } from "../../providers/TipsContext";
 
 
 const { width, height } = Dimensions.get("screen");
 
-const Tips = () => {
-  const [nombre, setNombre] = useState("");
-  const [edad, setEdad] = useState("");
-  const [raza, setRaza] = useState("");
-  const [descripcion, setDescripcion] = useState("");
-  const [notas, setNotas] = useState("");
+const Tips = ({navigation}) => {
+  const { state } = useContext(AuthContext);
+  const { state: tipsState, getTips, createTip,deleteTip} = useContext(TipsContext);
+  useEffect(() => {
+    getTips();
+  }, []);
+  const [comentario, setComentario] = useState("");
+  console.log(tipsState.currentTip);
 
  return (
     <View>
         <Logo/>
-        <Text style={styles.text} h5>Ingresa tu mascota!</Text>
+        <Text >Tips</Text>
       <Input
-        placeholder="Nombre"
+        placeholder="Comentario"
         leftIcon={<FontAwesome5 name="dog" size={24} color="black" />}
-        value={nombre}
-        onChangeText={setNombre}
+        value={comentario}
+        onChangeText={setComentario}
 
       />
-      <Input
-        placeholder="Edad"
-        leftIcon={<Fontisto name="date" size={24} color="black" />}
-        value={edad}
-        onChangeText={setEdad}
-      />
-      <Input
-        placeholder="Raza"
-        leftIcon={<Entypo name="heart" size={24} color="black" />}
-        value={raza}
-        onChangeText={setRaza}
+      <Button title="agregar comentario"  onPress={() => {
+             createTip(comentario, state.user.user);
+            }} />
+      <Button title="borrar comentario"  onPress={() => {
+             deleteTip(tipsState.currentTip);
+            }} />
 
-      />
-      <Input
-        placeholder="Descripción"
-        leftIcon={<Feather name="align-justify" size={24} color="black" />}
-        value={descripcion}
-        onChangeText={setDescripcion}
-      />
-      <Input
-        placeholder="Notas"
-        leftIcon={<Entypo name="new-message" size={24} color="black" />}
-        value={notas}
-        onChangeText={setNotas}
-
-      />
-      <Button title="Crear Cuenta"  />
-      <Text style={styles.text}>Ó</Text>
-      <Button title='Google' />
+      <CommentList comments={tipsState.tips}/>
+      
     </View>
   );
 };
